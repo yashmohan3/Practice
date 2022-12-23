7c712945a75a702cad3d0126b2851d5806100bca
Yash Mohan <ymohan@axway.com>
2022-12-23 18:17:32 +0530

    shell scipt

---
+++ shell.sh
@@ -0,0 +1,15 @@
+#Create a script to find the files with a particular name and move those files to another folder
+#read -p "name: " filename
+#find . -type f -name $filename -exec cp {} ~/Documents/new \;
+
+#Create a script to read a file containing the name of the files that we want to move. If in the list name is present, then move the file else display no such file in the directory.
+#read -p "name: " filename
+# grep -ir 'New.txt' -e $filename
+# if [ $? = 0 ]
+# then
+#     find . -type f -name $filename -exec cp {} ~/Documents/new \;
+# else
+#     echo "No such file in directory"
+# fi
+
+
